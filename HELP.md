0df2e80a681d12409c276db42ca027b5077bf380
Eddie Wettach <ewettach@EW4-M1-MacBook-Pro.local>
2022-04-24 22:57:04 -0500

    v1.0.1

--- HELP.md
+++ HELP.md
@@ -1,19 +1,22 @@
 # companion-module-daktronics-showcontrol
 # Daktronics Show Control Client Module for Companion
-v 1.0.0
+v 1.0.1
 4/29/19
 Eddie Wettach <ewettach@gmail.com>
 
 ## Change Log
+1.0.1 - Added some customization and default options in the settings of the module.
+        Added logging to see what is being played out to troubleshoot path/filename issues.
 1.0.0 - Initial Support - play still images/videos and blank display
 
 ## Requirements:
 1.  Companion
-2.  Daktronics DMP (Tested with DMP 8000)
+2.  Daktronics DMP (Tested with DMP 8000 v1.37.0.32, v2.34.0.115, but probably works with others)
 
 ## How to:
 1.  Add an instance of Show Control Client to Companion.
-2.  Configure the IP address of the Daktronics DMP
+2.  Configure the IP address of the Daktronics DMP and port number if needed
+3.  Set optional starting paths and sign id if desired
 3.  Configure a button with the action Play File or Blank Display
 4.  Follow more specific steps below under "Actions" depending on what you selected in the above #4
 
@@ -25,8 +28,9 @@ Allows user to play a still image or video.
 
 #### Parameters:
 
-- **Filename**: The path to the file to be played (i.e C:/images/sample.png)
-- **Sign ID**: The id of the sign to be played on (i.e. 264x480:primary/fullscreen)
+- **Absolute File Path**: The absolute path to the file to be played (i.e C:/data/pictures/sample.png).  Will override relative file path if set.
+- **Relative File Path**: The relative path to the file to be played (i.e pictures/sample.png).  This assumes that a default path is set in the settings.
+- **Sign ID**: The id of the sign to be played on (i.e. 264x480:primary/fullscreen).  Leave blank to use the default in settings.
 
 
 
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var instance_skel = require('../../instance_skel');
 var http = require('http');
+const { lowerFirst } = require('lodash');
 var debug;
 var log;
 
@@ -26,14 +27,18 @@ class instance extends instance_skel {
 							options: [
 								{
 									type:  'textinput',
-									label: 'Filename',
+									label: 'Absolute Filepath and filename. Will over ride relative path if set.',
 									id:    'file_name'
 								},
 								{
 									type:  'textinput',
-									label: 'Sign ID',
-									id:    'sign_id',
-									default: '264x480:primary/fullscreen'
+									label: 'Relative Filepath and filename. Assumes a default path is set in settings.',
+									id:    'relative_name'
+								},
+								{
+									type:  'textinput',
+									label: 'Sign ID (leave blank to use default set in settings)',
+									id:    'sign_id'
 								}
 							]
 						},
@@ -42,9 +47,8 @@ class instance extends instance_skel {
 							options: [
 								{
 									type:  'textinput',
-									label: 'Sign ID',
-									id:    'sign_id',
-									default: '264x480:primary/fullscreen'
+									label: 'Sign ID (leave blank to use default set in settings)',
+									id:    'sign_id'
 								}
 							]
 						}			
@@ -54,19 +58,31 @@ class instance extends instance_skel {
 		var self = this;
 		var url = 'http://' + self.config.host + ':' + self.config.port;
 		var sign_id=action.options.sign_id;
-
+		if (!sign_id) {
+			sign_id = self.config.default_sign_id;
+		}
 		// Create http request client
 		var request = require("request")
 		var envelope_header='<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"><s:Header><h:PlayControlOperationHeader xmlns:h="http://standards.daktronics.com/schemas/playerservices/playercontrol_1_0.wsdl" xmlns="http://standards.daktronics.com/schemas/playerservices/playercontrol_1_0.wsdl" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema"/></s:Header><s:Body xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">';
 		var envelope_footer='</s:Body></s:Envelope>';
 
-		switch (action.action){
+		switch (action.action) {
 			case 'play_file':
-				var unique_file_name = action.options.file_name;
-				var body = envelope_header + '<Play xmlns="http://standards.daktronics.com/schemas/playerservices/playercontrol_1_0.wsdl"><sign><Mode>Name</Mode><SignId>' + sign_id + '</SignId></sign><options><File><Mode>UniqueFilename</Mode><UniqueFilename>' + unique_file_name + '</UniqueFilename></File><PlayMode>Continuous</PlayMode></options></Play>' + envelope_footer;					
+				// If the absolute path and filename is set, then use it, otherwise use the relative path and filename.
+				if (action.options.file_name) {
+					var unique_file_name = action.options.file_name;
+				} else {
+					var unique_file_name = self.config.default_path + action.options.relative_name;
+				}
+				// for adding double backslashes in the filepath and filename 
+				let regex = /\\/g;
+
+				var body = envelope_header + '<Play xmlns="http://standards.daktronics.com/schemas/playerservices/playercontrol_1_0.wsdl"><sign><Mode>Name</Mode><SignId>' + sign_id + '</SignId></sign><options><File><Mode>UniqueFilename</Mode><UniqueFilename>' + unique_file_name.replace(regex, "\\\\") + '</UniqueFilename></File><PlayMode>Continuous</PlayMode></options></Play>' + envelope_footer;
+				this.log("info", "Playing File: " + unique_file_name + " on sign id: " + sign_id);
 			break
 			case 'blank_display':
-			var body = envelope_header + '<Blank xmlns="http://standards.daktronics.com/schemas/playerservices/playercontrol_1_0.wsdl"><sign><Mode>Name</Mode><SignId>' + sign_id + '</SignId></sign></Blank>' + envelope_footer;					
+				var body = envelope_header + '<Blank xmlns="http://standards.daktronics.com/schemas/playerservices/playercontrol_1_0.wsdl"><sign><Mode>Name</Mode><SignId>' + sign_id + '</SignId></sign></Blank>' + envelope_footer;					
+				this.log("info", "Blanking Display: " + sign_id);
 			break
 		}
 
@@ -106,20 +122,80 @@ class instance extends instance_skel {
 		var self = this;
 		return [
 			{
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: '',
+				value: "<br>", // space to separate settings
+			},
+			{
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: 'IP Address and Port Settings',
+				value:
+						'Below set the IP address and port of the Daktronics DMP 8000.   The default port is 4502.',
+			},
+			{
 				type:    'textinput',
 				id:      'host',
-				label:   'DMP 8000 IP Address',
-				tooltip: 'The IP of the Daktronics DMP 8000 Server',
-				width:   6,
+				label:   'DMP IP Address',
+				tooltip: 'The IP of the Daktronics DMP Server',
+				width:   8,
 				regex:   self.REGEX_IP
 			},
 			{
 				type:    'textinput',
 				id:      'port',
-				label:   'Daktronics DMP 8000 Port Number (default 4502)',
-				tooltip: 'The Port Number that Daktronics DMP 8000 is listening on.',
-				width:   6,
+				label:   'DMP Port Number',
+				tooltip: 'The Port Number that Daktronics DMP is listening on.',
+				width:   4,
 				default: '4502'
+			},
+			{
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: '',
+				value: "<br>", // space to separate settings
+			},
+			{
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: 'OPTIONAL SETTINGS',
+				value: '',
+			},
+				{
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: 'Default Path (Optional)',
+				value:
+					'Below set the default path for files that are to be played; i.e. <b>C:\\DaktronicsData\\Videos\\</b><br>If default path is set, then the button instances should only use relative paths.',
+			},
+			{
+				type:    'textinput',
+				id:      'default_path',
+				label:   'Default Path of files stored on DMP.',
+				tooltip: "i.e. C:\\Data\\  (If left blank, specify the whole path on each button instance)",
+				width:   12,
+				default: ''
+			},{
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: 'Sign ID (Optional)',
+				value:
+							'Below set the default sign id, i.e. <b>264x480:primary/fullscreen</b><br>If the default sign id is set, then button instances that do not specify a sign id will use the default set here. ',
+			},
+			{
+				type:    'textinput',
+				id:      'default_sign_id',
+				label:   'Default Sign ID on DMP.',
+				tooltip: "i.e. 264x480:primary/fullscreen  (If left blank, specify the sign id on each button instance)",
+				width:   8,
+				default: ''
 			}
 		]
 	}
